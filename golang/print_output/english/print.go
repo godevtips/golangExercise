3fad3b6d7e099b48872dfae26f7df866f497a73b
Melchior Vrolijk <vrol0004@gmail.com>
2021-08-28 11:34:17 +0200

    print examples added

---
+++ golang/print_output/english/print.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Go dev tips ","is ","awesome") // output -> Go dev tips is awesome
+	fmt.Print(123,4,5) // output -> 123 4 5
+}
---
+++ golang/print_output/english/print_f.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	stringValue := "Go dev tips!"
+	fmt.Printf("Hello %s",stringValue)
+}
