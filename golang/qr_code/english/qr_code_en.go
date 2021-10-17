c6b3a9e41edabd157d571e052f723a86dcaa23ca
mel182 <vrol0004@gmail.com>
2021-10-17 13:08:32 +0200

    Qr code example added

--- golang/qr_code/english/qr_code_en.go
+++ golang/qr_code/english/qr_code_en.go
@@ -10,13 +10,14 @@ import (
 	"os"
 )
 
-var websiteURL = "https://www.godevtips.com"
-var imageSize = 256
+var websiteURL = "https://www.godevtips.com" // GoDevTips URL
+var imageSize = 256                          // 256 x 256 pixels
 
 func main() {
 
-	if len(os.Args) == 2 && os.Args[1] == "file" { // Determine which is option must be launch
+	if len(os.Args) == 2 && os.Args[1] == "file" { // Determine which is option to launch
 
+		// Generate only the raw PNG bytes
 		outputFileName := "qr_code.png"
 		taskError := qrcode.WriteFile(websiteURL, qrcode.Medium, imageSize, outputFileName)
 
@@ -32,8 +33,8 @@ func main() {
 
 func startServer() {
 
-	router := httprouter.New()
-	router.GET("/", sendQRCode)
+	router := httprouter.New()  // router instance
+	router.GET("/", sendQRCode) // landing page
 
 	fmt.Println("server listening on port 3000")
 	serverError := http.ListenAndServe(":3000", router)
@@ -44,9 +45,15 @@ func startServer() {
 
 func sendQRCode(response http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 
+	// Generate QR code HTML image tag
 	qr_code_image_tag := generateQRCodeHtmlImageTag()
+	// Set response header to Status Ok (200)
 	response.WriteHeader(http.StatusOK)
+
+	// HTML image with embedded QR code
 	responsePageHtml := "<!DOCTYPE html><html><body><h1>QR Code example Go Dev Tips</h1>" + qr_code_image_tag + "</body></html>"
+
+	// Send HTML response back to client
 	_, _ = response.Write([]byte(responsePageHtml))
 }
 
@@ -58,6 +65,7 @@ func generateQRCodeHtmlImageTag() string {
 		log.Fatalln("Error generating QR code. ", taskError)
 	}
 
+	// // Encode raw QR code data to base 64
 	encodedData := base64.StdEncoding.EncodeToString(qrCodeImageData)
 
 	return "<img src=\"data:image/png;base64, " + encodedData + "\">"
--- golang/qr_code/espanol/qr_code_es.go
+++ golang/qr_code/espanol/qr_code_es.go
@@ -17,6 +17,7 @@ func main() {
 
 	if len(os.Args) == 2 && os.Args[1] == "archivo" { // Determine cuál opción que debe lanzarse
 
+		// Genere solo los bytes PNG sin procesar
 		nombreDelArchivo := "qr_code.png"
 		_error := qrcode.WriteFile(urlDelSitioWeb, qrcode.Medium, tamanoDeImagen, nombreDelArchivo)
 
@@ -32,8 +33,8 @@ func main() {
 
 func iniciarServidor() {
 
-	enrutador := httprouter.New()
-	enrutador.GET("/", enviarCodigoQR)
+	enrutador := httprouter.New()      // instancia de enrutador
+	enrutador.GET("/", enviarCodigoQR) // página de destino
 
 	fmt.Println("servidor escuchando atraves del puerto 3000")
 	errorDelServidor := http.ListenAndServe(":3000", enrutador)
@@ -44,9 +45,16 @@ func iniciarServidor() {
 
 func enviarCodigoQR(respuesta http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 
+	// Generar etiqueta de imagen HTML de código QR
 	etiqueta_de_imagen_de_codigo_qr := generarEtiquetaDeImagenHtmlDeCodigoQR()
+
+	// Establecer el encabezado de respuesta en Estado Ok (200)
 	respuesta.WriteHeader(http.StatusOK)
+
+	// Imagen HTML con código QR incrustado
 	paginaDeRepuestaHtml := "<!DOCTYPE html><html><body><h1>Ejemplo de código QR Go Dev Tips</h1>" + etiqueta_de_imagen_de_codigo_qr + "</body></html>"
+
+	// Enviar respuesta HTML
 	_, _ = respuesta.Write([]byte(paginaDeRepuestaHtml))
 }
 
